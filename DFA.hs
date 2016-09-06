7240a826937b082e8051a5ff87d18696af70dbb7
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2016-09-06 08:42:05 +0100

    Added product automata and DFA union

--- DFA.hs
+++ DFA.hs
@@ -1,33 +1,59 @@
+import Data.List (union)
+
 -- Based on: https://cseweb.ucsd.edu/classes/wi14/cse105-a/haskell/intro.html<Paste>
 data DFA q s = DFA 
-  {states :: [q]
-  ,inputs :: [s]
-  ,transition :: (q -> s -> q)
-  ,initial :: q
-  ,final :: [q]
+  {getStates :: [q]
+  ,getInputs :: [s]
+  ,getTransition :: (q -> s -> q)
+  ,getInitial :: q
+  ,getFinal :: [q]
   }
 -- where q refers to some finite set of states
 -- and s the input alphabet (a finite set of symbols)
 
 evaluate :: (Eq q) => DFA q s -> [s] -> Bool
-evaluate dfa str = foldl f (initial dfa) str `elem` final dfa
-  where f  = transition dfa
-
+evaluate dfa str = foldl f (getInitial dfa) str `elem` getFinal dfa
+  where f  = getTransition dfa
 
 -- Test with a simple automaton to accept the language 0*1*
-states' = [0, 1, 2]
-dead = 2
+states1 = [0, 1, 2]
+dead1 = 2
 
-inputs' = [0, 1]
-final' = [0, 1]
-initial' = 0
-transition' :: Int -> Int -> Int
-transition' 0 0 = 0
-transition' 0 1 = 1
-transition' 1 0 = dead
-transition' 1 1 = 1
+inputs1 = [0, 1]
+final1 = [0, 1]
+initial1 = 0
+transition1 :: Int -> Int -> Int
+transition1 0 0 = 0
+transition1 0 1 = 1
+transition1 1 1 = 1
+transition1 _ _ = dead1
 
-dfa' = DFA states' inputs' transition' initial' final'
+dfa1 = DFA states1 inputs1 transition1 initial1 final1
+
+-- Another automaton to accept the language 00*10
+states2 = [0, 1, 2, 3, 4]
+dead2 = 2
+
+inputs2 = [0, 1]
+final2 = [4]
+initial2 = 0
+transition2 :: Int -> Int -> Int
+transition2 0 0 = 3
+transition2 3 0 = 3
+transition2 3 1 = 1
+transition2 1 0 = 4
+transition2 _ _ = dead2
+
+dfa2 = DFA states2 inputs2 transition2 initial2 final2
+
+createProductAutomaton :: DFA q1 s -> DFA q2 s -> [(q1,q2)] -> DFA (q1,q2) s
+createProductAutomaton a b finals = DFA allStates (getInputs a) transitionF (getInitial a, getInitial b) finals
+  where allStates = [(x,y) | x <- (getStates a), y <- (getStates b)]
+        transitionF (q1,q2) s = (getTransition a q1 s, getTransition b q2 s)
+
+unionDFA a b = createProductAutomaton a b allFinals
+  where allFinals = union [(x,y) | x <- (getFinal a), y <- (getStates b)] [(x,y) | x <- (getStates a), y <- (getFinal b)]
 
 main = do
-  print $ evaluate dfa' [0,1,0] 
+--  print $ evaluate dfa1 [0,1,1] 
+  print $ evaluate (unionDFA dfa1 dfa2) [0,0,1,0,0] 
