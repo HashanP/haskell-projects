c26cf3b60dda11808af18b238f45fce62937b001
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2016-11-15 19:25:34 +0000

    Updated Differentation.hs with new symbolic data structure

--- Differentiation.hs
+++ Differentiation.hs
@@ -1,46 +1,83 @@
 {-
 Inspired by: http://5outh.blogspot.co.uk/2013/05/symbolic-calculus-in-haskell.html
 -}
-
-infixl 4 :+
-infixl 5 :*, :/
-infixl 6 :^
+import Data.List (intersperse)
 
 data Sym a
-  = Const Int
-  | Var Char
-  | (Sym a) :+ (Sym a)
-  | (Sym a) :* (Sym a)
-  | (Sym a) :/ (Sym a)
-  | (Sym a) :^ (Sym a)
+  = Var Char
+  | Sum [Sym a] Int
+  | Product [Sym a]
+  | Exponentation (Sym a) (Sym a)
   deriving (Show, Eq)
 
-diff :: Char -> Sym a -> Sym a
-diff x (Const _) = Const 0
+simplify :: Sym a -> Sym a
 
-diff x (Var y)
-  | x == y = Const 1
-  | x /= y = Var y
+simplify (Sum [x] 0) = simplify x
 
--- Chain rule
-diff z (x :^ y) = (y :* (x :^ (y :+ (Const (-1))))) :* (diff z x)
+simplify (Sum xs const) = Sum (invalid ++ extractTerms) (const + extractConst)
+  where pred (Sum _ _) = True
+        pred _ = False
+        valid = filter pred subCleaned
+        invalid = filter (not . pred) subCleaned
+        extractTerms = concat $ map (\(Sum xs const) -> xs) valid
+        extractConst = foldl (+) 0 $ map (\(Sum xs const) -> const) valid
+        subCleaned = map simplify xs
 
--- Product rule
-diff z (u :* v) = (u :* (diff z v)) :+ (v :* (diff z u))
+simplify (Product [x]) = simplify x
 
--- Quotient rule
-diff z (u :/ v) = ((v :* (diff z u)) :+ ((Const (-1)) :* u :* (diff z v)) :/ (v :^ (Const 2)))
+simplify (Product xs) = Product (getNotConst ++ extractTerms ++ memy2)
+  where pred (Product _) = True
+        pred _ = False
+        valid = filter pred subCleaned
+        invalid = filter (not . pred) subCleaned
+        extractTerms = concat $ map (\(Product xs) -> xs) valid
+        subCleaned = map simplify xs
+        getConst = filter pred2 invalid
+        getNotConst = filter (not . pred2) invalid
+        pred2 (Sum [] _) = True
+        pred2 _ = False
+        mapsy (Sum [] n) = n
+        memy = foldl (*) 1 $ map mapsy getConst
+        memy2
+          | memy == 1 = []
+          | otherwise = [Sum [] memy]
 
--- Distributive over addition (linear operator)
-diff z (x :+ y) = (diff z x) :+ (diff z y)
+simplify (Exponentation x y)
+  | y' == (Sum [] 1) = x'
+  | otherwise = (Exponentation x' y')
+  where x' = simplify x
+        y' = simplify y
 
-simplify ((Const x) :+ (Const y)) = Const (x + y)
-simplify ((Const 1) :* x) = (simplify x) 
-simplify (x :* (Const 1)) = (simplify x) 
-simplify (x :^ (Const 1)) = (simplify x) 
-simplify (x :^ y) = (simplify x) :^ (simplify y)
-simplify (x :* y) = (simplify x) :* (simplify y)
 simplify x = x
 
-main = do
-  print $ simplify $ diff 'x' ((Var 'x') :^ (Const 2))
+diff :: Char -> Sym a -> Sym a
+
+-- Differentiating variables
+diff x (Var y)
+  | x == y = (Sum [] 1)
+  | otherwise = (Sum [] 0)
+
+-- Power rule -- Doesn't work yet if you have a funciton of x as the exponent
+diff x (Exponentation base exponent) = Product [(Exponentation base (Sum [exponent] (-1))), exponent]
+
+-- Distributive over addition (Linear operator)
+diff x (Sum xs _) = Sum (map (diff x) xs) 0
+
+-- Product rule
+diff z (Product xs) = Product [Sum (map term [0..(length xs - 1)]) 0]
+  where term x = Product ((take x xs) ++ [diff z (xs !! x)] ++ (drop (x+1) xs))
+
+pretty :: Sym a -> String
+
+pretty (Var x) = [x]
+
+pretty (Sum xs x)
+  | m == [] && x == 0 = ""
+  | m == [] = (show x)
+  | x == 0 = (concat $ intersperse " + " m)
+  | otherwise = (show x) ++ " + " ++ (concat $ intersperse " + " m)
+  where m = map pretty xs
+
+pretty (Exponentation x y) = "(" ++ (pretty x) ++ ") ^ (" ++ (pretty y) ++ ")"
+
+pretty (Product xs) = "(" ++ (concat $ intersperse ")(" (map pretty xs)) ++ ")"
