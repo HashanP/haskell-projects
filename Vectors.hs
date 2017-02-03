717e21f6935abd2be7e9398065b18748397f3ab1
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2017-02-03 17:20:27 +0000

    Added isRightAngle function

--- Vectors.hs
+++ Vectors.hs
@@ -3,10 +3,13 @@ magnitude :: (Floating a, Num a) => [a] -> a
 magnitude = sqrt . sum . map (** 2)
 
 -- Dot Product
-dot :: (Floating a, Num a) => [a] -> [a] -> a
+dot :: (Num a) => [a] -> [a] -> a
 dot [] [] = 0
 dot (x: xs) (y: ys) = (x * y) + (dot xs ys)
 
 -- Angle
 angle :: (Floating a, Num a) => [a] -> [a] -> a
 angle a b = acos $ (dot a b) / ((magnitude a) * (magnitude b))
+
+isRightAngle :: (Eq a, Num a) => [a] -> [a] -> Bool
+isRightAngle a b = (dot a b) == 0
